4d8757be90f0791812a6f99a2ef0b8587ff73b66
Lars Trieloff <trieloff@adobe.com>
2019-10-24 08:25:53 +0000

    refactor(fields): move fields implementation into separate file

--- src/index.js
+++ src/index.js
@@ -12,40 +12,6 @@
 const traverse = require('traverse');
 const pointer = require('json-pointer');
 const { match } = require('./match');
-
-function filterFields(obj, fields = []) {
-  if (fields.filter((exp) => exp === '*').length > 0 || fields.length === 0) {
-    return obj; // nothing to filter for, we can skip
-  }
-
-  traverse(obj).forEach(function handle() {
-    const matches = fields.filter((pattern) => match(this.path, pattern)).length;
-    if (!matches) {
-      pointer.remove(obj, `/${this.path.join('/')}`);
-    }
-  });
-
-  return obj;
-}
-
-function extractFields(headers) {
-  if (headers.fields && Array.isArray(headers.fields)) {
-    return headers.fields;
-  } else if (headers.fields) {
-    return [headers.fields];
-  }
-  return ['*']; // allow all
-}
-
-function wrapFields(fn) {
-  return async (params) => {
-    const retval = await fn(params);
-    const fields = params && params.__ow_headers ? extractFields(params.__ow_headers) : ['*'];
-    if (retval.body && typeof retval.body === 'object') {
-      retval.body = filterFields(retval.body, fields);
-    }
-    return retval;
-  };
-}
+const { filterFields, wrapFields } = require('./fields');
 
 module.exports = { filterFields, wrapFields, match };
